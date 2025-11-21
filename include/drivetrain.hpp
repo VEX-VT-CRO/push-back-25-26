0f87a903e762472cb02615c83d0661826c304649
AuthXD <komaltummala2006@outlook.com>
2025-11-21 15:10:03 -0500

    arcade drive with lemlib done

--- include/drivetrain.hpp
+++ include/drivetrain.hpp
@@ -1,39 +1,74 @@
 #ifndef __DRIVETRAIN_HPP__
 #define __DRIVETRAIN_HPP__
+
 #include "api.h"
+#include "main.h"
+#include "lemlib/api.hpp"
 
 class Drivetrain {
-    // Constructor for an 8-motor tank; adjust ports/signs to your build.
-    public: 
+public:
+    // Constructor
     Drivetrain(
-        int fl, int mfl, int mbl, int bl,
-        int fr, int mfr, int mbr, int br,
+        int fl, int mfl, int mbl, int bl,     // Left motors
+        int fr, int mfr, int mbr, int br,     // Right motors
+        int imuPort,                           // IMU port
+        int leftEncPort,                       // Left rotation sensor port
+        int rightEncPort,                      // Right rotation sensor port
         pros::motor_brake_mode_e_t brakeMode = pros::E_MOTOR_BRAKE_BRAKE
-    )   
-        // Motor rotations so that positive moves forward and vice versa
-        : left({(int8_t)-fl, (int8_t)-mfl, (int8_t)mbl, (int8_t)bl})
-        , right({(int8_t)fr, (int8_t)mfr, (int8_t)-mbr, (int8_t)-br})
-    {
-        left.set_brake_mode_all(brakeMode);
-        right.set_brake_mode_all(brakeMode);
-    }
+    );
 
-    // Basic drive
-    void tank(int leftPower, int rightPower) {
-        left.move(leftPower);
-        right.move(rightPower);
-    }
+    // Manual drive
+    void tank(int leftPower, int rightPower, int deadband = 0);
+    void arcade(int forward, int turn, int deadband = 0);
 
-    // Direct control helpers
-    void setBrakeMode(pros::motor_brake_mode_e_t mode) {
-        left.set_brake_mode_all(mode);
-        right.set_brake_mode_all(mode);
-    }
+    // Pose and movement
+    void setPose(float x, float y, float heading);
+    void moveTo(float x, float y, int timeout);
+    void turnToHeading(float heading, int timeout);
+    void turnToPoint(float x, float y, int timeout);
+
+    // Calibration
+    void calibrate();
+
+    // Access underlying LemLib chassis
+    lemlib::Chassis* getChassis();
 
 private:
-    pros::MotorGroup left;
-    pros::MotorGroup right;
-};
+    // -----------------
+    // Motors
+    // -----------------
+    pros::Motor frontLeft;
+    pros::Motor middleFrontLeft;
+    pros::Motor middleBackLeft;
+    pros::Motor backLeft;
 
+    pros::Motor frontRight;
+    pros::Motor middleFrontRight;
+    pros::Motor middleBackRight;
+    pros::Motor backRight;
+
+    pros::MotorGroup leftMotors;
+    pros::MotorGroup rightMotors;
+
+    // -----------------
+    // Sensors
+    // -----------------
+    pros::Imu imu;
+    pros::Rotation leftEnc;
+    pros::Rotation rightEnc;
+
+    // -----------------
+    // LemLib Tracking Wheels
+    // -----------------
+    lemlib::TrackingWheel leftWheel;
+    lemlib::TrackingWheel rightWheel;
+
+    // -----------------
+    // LemLib Objects
+    // -----------------
+    lemlib::OdomSensors odomSensors;
+    lemlib::Drivetrain drivetrainCfg;
+    lemlib::Chassis chassis;
+};
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,31 @@
 #include "Drivetrain.hpp"
 
 //CONSTANTS GO HERE
-constexpr int EXAMPLE_PORT = 5;
+constexpr int FRONT_LEFT_PORT         = 2;
+constexpr int MIDDLE_FRONT_LEFT_PORT  = 3;
+constexpr int MIDDLE_BACK_LEFT_PORT   = 4;
+constexpr int BACK_LEFT_PORT          = 5;
+
+constexpr int FRONT_RIGHT_PORT        = 9;
+constexpr int MIDDLE_FRONT_RIGHT_PORT = 8;
+constexpr int MIDDLE_BACK_RIGHT_PORT  = 7;
+constexpr int BACK_RIGHT_PORT         = 6;
+
+constexpr int IMU_PORT = 10;
+constexpr int LEFT_ENCODER_PORT        = 1; 
+constexpr int RIGHT_ENCODER_PORT       = 2;
 
 //INITIALIZE SUBSYSTEMS HERE
 pros::Controller driver(pros::E_CONTROLLER_MASTER);
+
+Drivetrain drive(
+    FRONT_LEFT_PORT, MIDDLE_FRONT_LEFT_PORT, MIDDLE_BACK_LEFT_PORT, BACK_LEFT_PORT,
+    FRONT_RIGHT_PORT, MIDDLE_FRONT_RIGHT_PORT, MIDDLE_BACK_RIGHT_PORT, BACK_RIGHT_PORT,
+    IMU_PORT,
+    LEFT_ENCODER_PORT,
+    RIGHT_ENCODER_PORT,
+    pros::E_MOTOR_BRAKE_BRAKE
+);
 
 /**
  * Runs initialization code. This occurs as soon as the program is started.
@@ -16,6 +37,8 @@ pros::Controller driver(pros::E_CONTROLLER_MASTER);
 void initialize() {
 	pros::lcd::initialize();
 	pros::lcd::set_text(1, "Hello PROS User!");
+    drive.calibrate();
+    pros::lcd::set_text(1, "Init done");
 }
 
 /**
@@ -63,9 +86,13 @@ void autonomous() {}
  * task, not resume it from where it left off.
  */
 void opcontrol() {
-	while (true) {
-        int l = driver.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
-        int r = driver.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_Y);
-        pros::delay(20);
+    while (true) {
+        int forward = driver.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
+        int turn    = driver.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_X);
+
+        // arcade drive
+        drive.arcade(forward, turn, 15);
+
+        pros::delay(10);
     }
 }
